74178ee0d26690dbff6d704b2e5046347831fb21
Miguel Escobar <mescoba1@uci.edu>
2017-06-03 16:13:33 -0700

    sleeppp

--- bottle.py
+++ bottle.py
@@ -1,9 +1,13 @@
 
 class Bottle:
   
-  __slots__ = ["bottleNum","bottleName", "bottleSize"]
+  __slots__ = ["bottleNum","bottleName", "bottleSize", "sleepConstant"]
   
   def __init__(self, index, name, size):
     self.bottleNum = index
     self.bottleName = name
     self.bottleSize = size
+    self.sleepConstant = 1
+    
+  def getSleepTime(liquidAmount):
+    return sleepConstant*liquidAmount
