c02fe97aff2ad9ce75a1a31ca7f004889cfeb13f
Miguel Escobar <mescoba1@uci.edu>
2017-06-03 16:33:21 -0700

    Update bottle.py

--- bottle.py
+++ bottle.py
@@ -1,12 +1,13 @@
 
 class Bottle:
   
-  __slots__ = ["bottleNum","bottleName", "bottleSize", "sleepConstant"]
+  __slots__ = ["bottleNum","bottleName", "bottleSize", "bottleAmount", "sleepConstant"]
   
   def __init__(self, index, name, size):
     self.bottleNum = index
     self.bottleName = name
     self.bottleSize = size
+    self.bottleAmount = size
     self.sleepConstant = 1
     
   def getSleepTime(liquidAmount):
